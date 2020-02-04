5a7eba783322679dde223cd83976aecb0da4ac87
Georgios Bakas <georgios.bakas@cern.ch>
2020-02-04 11:43:51 +0200

    new files and new mass fit. Need to run one more file, missing 1

--- MassFit/Mixed/TemplateConstants.h
+++ MassFit/Mixed/TemplateConstants.h
@@ -90,12 +90,12 @@ void initFilesMapping()
 	variableConstant.insert(pair<TString, int>("jetY1", 6));
 
 	//these are fit results from mixed medium wp and loose wp
-	Nbkg2Constants.insert(pair<TString, float>("Nbkg2016", 2.7195e+03));
-	Nbkg2Constants.insert(pair<TString, float>("Nbkg2017", 2.6515e+03));
+	Nbkg2Constants.insert(pair<TString, float>("Nbkg2016", 2.7202e+03));
+	Nbkg2Constants.insert(pair<TString, float>("Nbkg2017", 2.6557e+03));
 	Nbkg2Constants.insert(pair<TString, float>("Nbkg2018", 4.7447e+03));
 
-	Nbkg2ConstantsErrors.insert(pair<TString, float>("Nbkg2016_error", 1.90e+02));
-	Nbkg2ConstantsErrors.insert(pair<TString, float>("Nbkg2017_error", 2.52e+02));
+	Nbkg2ConstantsErrors.insert(pair<TString, float>("Nbkg2016_error", 1.91e+02));
+	Nbkg2ConstantsErrors.insert(pair<TString, float>("Nbkg2017_error", 2.55e+02));
 	Nbkg2ConstantsErrors.insert(pair<TString, float>("Nbkg2018_error", 3.27e+02));
 	/*
 	BND[variableConstant["mJJ"]].push_back({1000, 1200, 1400, 1600, 1800, 2000, 2400, 2800, 3200, 4000, 5000}); //mjj
