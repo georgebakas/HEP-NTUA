b5c8198fbc4853245dfa5b5ef036216601dbe6ca
George Bakas <georgios.bakas@cern.ch>
2020-04-01 10:09:27 +0200

    re-ran data fill histogram for both methods, to include fabs(jetY) leading and subleading

--- MassFit/FillHistoCodes_Jan2020/FillHistograms.C
+++ MassFit/FillHistoCodes_Jan2020/FillHistograms.C
@@ -270,14 +270,14 @@ void FillHistograms(TString y="2016", int sel = 0, bool isLoose=false)
   cout<<"Entering "<<eosPath+listOfFiles[f]<<endl;
   inf = TFile::Open(eosPath+listOfFiles[f]);   
   TTree *trIN    = (TTree*)inf->Get("boosted/events");  
-  cout<<"XSEC: "<<XSEC[f]<<endl; 
+  //cout<<"XSEC: "<<XSEC[f]<<endl; 
   if(selection != 0)
   {
     float NORM = ((TH1F*)inf->Get("eventCounter/GenEventWeight"))->GetSumOfWeights(); 
     weights.push_back(XSEC[f]/NORM);  
   } 
   cout<<"file: "<<eosPath+listOfFiles[f]<<endl;
-  cout<<"weight: "<<weights[f]<<endl;
+  //cout<<"weight: "<<weights[f]<<endl;
   cout<<"LUMI: "<<LUMI<<endl;
   cout<<"LUMI_CR: "<<LUMI_CR<<endl;
   int decade(0);
