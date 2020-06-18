e2d02336478b8dab616e19afdec91f6ff0e1a127
Georgios Bakas <georgios.bakas@cern.ch>
2020-06-18 17:18:14 +0300

    retry ptJJ and mJJ

--- MassFit/FillHistoCodes_Jan2020/FillHistograms_Reduced_UnequalBinning.cpp
+++ MassFit/FillHistoCodes_Jan2020/FillHistograms_Reduced_UnequalBinning.cpp
@@ -227,8 +227,8 @@ void FillHistograms_Reduced_UnequalBinning(TString y="2016", int sel = 0, bool i
 
   gStyle->SetOptStat(0);
   const int NVAR =11;
-  const int N_MJJ = 9;
-  const int N_PTJJ = 7;
+  const int N_MJJ = 10;
+  const int N_PTJJ = 8;
   const int N_YJJ = 8;
   const int N_PT0 = 20;
   const int N_PT1 = 9;
@@ -244,13 +244,15 @@ void FillHistograms_Reduced_UnequalBinning(TString y="2016", int sel = 0, bool i
 
   int NBINS[NVAR] = {N_MJJ, N_PTJJ, N_YJJ, N_PT0, N_PT1 ,N_JETY, N_JETY,N_MVA, N_MVA ,N_JETMASS, N_JETMASS};
 
-  std::vector< std::vector <Float_t> > const BND = {{1000, 1200, 1400, 1600, 1800, 2000, 2400, 2800, 3700, 5000}, //mjj
-													{0,60,150,300,450,650,900,1300}, //ptjj
-													{-2.4,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.4}, //yjj
-		   	                                        {400,425,450,475,500,535,570,610,650,700,750,800,850,900,950,1025,1100,1200,1300,1400,1500}, //jetPt0 21
-													{400,450,500,570,650,750,850,1000,1200,1500}, //jetPt1
-													{0.0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4}, //jetY0
+  std::vector< std::vector <Float_t> > const BND = {{1000, 1200, 1400, 1600, 1800, 2000, 2400, 2800, 3200, 3700, 5000}, //mjj
+													                          {0,60,150,300,450,650,800,1100,1300}, //ptjj
+													                          {-2.4,-1.5,-1.0,-0.5,0.0,0.5,1.0,1.5,2.4}, //yjj
+		   	                                            {400,425,450,475,500,535,570,610,650,700,750,800,850,900,950,1025,1100,1200,1300,1400,1500}, //jetPt0 21
+													                          {400,450,500,570,650,750,850,1000,1200,1500}, //jetPt1
+													                          {0.0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4}, //jetY0
                                                     {0.0,0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4}}; //jetY1
+
+
 
   TString varReco[NVAR]   = {"mJJ", "ptJJ", "yJJ","jetPt0","jetPt1", "jetY0", "jetY1",
                "mva", "topTagger1", "mTop", "jetMassSoftDrop"};
