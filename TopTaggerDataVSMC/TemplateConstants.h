414d20943fd71b743a9db22b27e811602ae7e17c
Georgios Bakas <georgios.bakas@cern.ch>
2020-08-31 16:19:50 +0300

    Ready (?) for presentation (btagging SF's vs no btag sf's)

--- TopTaggerDataVSMC/TemplateConstants.h
+++ TopTaggerDataVSMC/TemplateConstants.h
@@ -336,18 +336,10 @@ void initFilesMapping(bool isLoose)
 	variableConstant.insert(pair<TString, int>("jetY0", 5));
 	variableConstant.insert(pair<TString, int>("jetY1", 6));
 
-	//these are fit results from mixed medium wp and loose wp
-	Nbkg2Constants.insert(pair<TString, float>("2016", 8.6815e+03));
-	Nbkg2Constants.insert(pair<TString, float>("2017", 5.2346e+03));
-	Nbkg2Constants.insert(pair<TString, float>("2018", 1.2031e+04));
 
-	Nbkg2ConstantsErrors.insert(pair<TString, float>("2016", 1.54e+03));
-	Nbkg2ConstantsErrors.insert(pair<TString, float>("2017", 9.25e+02));
-	Nbkg2ConstantsErrors.insert(pair<TString, float>("2018", 5.52e+02));
-
-	signalStrenth.insert(pair<TString, float>("2016", 0.784403));
-	signalStrenth.insert(pair<TString, float>("2017", 0.579506));
-	signalStrenth.insert(pair<TString, float>("2018", 0.681708));
+	signalStrenth.insert(pair<TString, float>("2016", 0.803012));
+	signalStrenth.insert(pair<TString, float>("2017", 0.675417));
+	signalStrenth.insert(pair<TString, float>("2018", 0.758664));
 
 	/*
 	BND[variableConstant["mJJ"]].push_back({1000, 1200, 1400, 1600, 1800, 2000, 2400, 2800, 3200, 4000, 5000}); //mjj
