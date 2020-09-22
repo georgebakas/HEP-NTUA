058b576df760c821c3aca44d22d354bf64a2a69f
George Bakas <georgios.bakas@cern.ch>
2020-09-22 13:55:49 +0200

    minor change for 2018

--- BTagScaleFactors/UpdateVariations.py
+++ BTagScaleFactors/UpdateVariations.py
@@ -11,8 +11,9 @@ allFiles = []
 for ifile, file_name in enumerate(glob.iglob('/eos/cms/store/user/ipapakri/ttbar/MC/Signal/{}/variations/*.root'.format(year), recursive=True)):
 	#print(file_name)
 	#print(f'root -l -b -q \'myMacro.C(\"{file_name}\")\'')
-	print("file {}".format(ifile))
-	split_file_name = file_name.split('/')
-	print(split_file_name[-1])
-	os.system(f'root -l -b -q \'UpdateBTagSF_Variations.C(\"{split_file_name[-1]}\", \"{year}\")\'')
+	if ifile > 21:
+		print("file {}".format(ifile))
+		split_file_name = file_name.split('/')
+		print(split_file_name[-1])
+		os.system(f'root -l -b -q \'UpdateBTagSF_Variations.C(\"{split_file_name[-1]}\", \"{year}\")\'')
 	#break
