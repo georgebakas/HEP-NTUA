aa0097a07171aed5e78e0cec39a4469d5ae61a4e
Georgios Bakas <georgios.bakas@cern.ch>
2020-09-23 10:01:30 +0300

    Submitted changes for the update of the top tagger for 2018

--- BTagScaleFactors/UpdateVariations.py
+++ BTagScaleFactors/UpdateVariations.py
@@ -1,6 +1,6 @@
-import glob 
+import glob
 import os
-import sys 
+import sys
 
 print(f'Number of arguments {len(sys.argv)}')
 print(f'Argument list: {str(sys.argv)}')
@@ -8,12 +8,13 @@ print(f'Argument list: {str(sys.argv)}')
 year = sys.argv[1]
 allFiles = []
 
-for ifile, file_name in enumerate(glob.iglob('/eos/cms/store/user/ipapakri/ttbar/MC/Signal/{}/variations/*.root'.format(year), recursive=True)):
+for ifile, file_name in enumerate(glob.iglob('/eos/cms/store/user/g/gbakas/ttbar/mc-{}/Signal/variations/*.root'.format(year), recursive=True)):
 	#print(file_name)
 	#print(f'root -l -b -q \'myMacro.C(\"{file_name}\")\'')
-	if ifile > 21:
-		print("file {}".format(ifile))
-		split_file_name = file_name.split('/')
-		print(split_file_name[-1])
-		os.system(f'root -l -b -q \'UpdateBTagSF_Variations.C(\"{split_file_name[-1]}\", \"{year}\")\'')
-	#break
+
+	print("file {}".format(ifile))
+	#split_file_name = file_name.split('/')
+	#print(split_file_name[-1])
+	print(file_name)
+	os.system(f'root -l -b -q \'UpdateTopTagger.C(\"{file_name}\")\'')
+	break
