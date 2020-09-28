e957211002ea25541c23cd375175f33bcea10c7a
George Bakas <georgios.bakas@cern.ch>
2020-09-28 08:47:24 +0200

    Added new angular files

--- BTagScaleFactors/UpdateVariations.py
+++ BTagScaleFactors/UpdateVariations.py
@@ -11,10 +11,10 @@ allFiles = []
 for ifile, file_name in enumerate(glob.iglob('/eos/cms/store/user/ipapakri/ttbar/MC/Signal/{}/variations/*.root'.format(year), recursive=True)):
 	#print(file_name)
 	#print(f'root -l -b -q \'myMacro.C(\"{file_name}\")\'')
-
-	print("file {}".format(ifile))
-	split_file_name = file_name.split('/')
-	print(split_file_name[0])
-	#print(file_name)
-	os.system(f'root -l -b -q \'UpdateBTagSF.C(\"{split_file_name[-1]}\", \"{year}\")\'')
-	#break
+	if ifile > 28:
+		print("file {}".format(ifile))
+		split_file_name = file_name.split('/')
+		print(split_file_name[0])
+		#print(file_name)
+		os.system(f'root -l -b -q \'UpdateBTagSF.C(\"{split_file_name[-1]}\", \"{year}\")\'')
+		#break
--- TopAngular/FillHistograms.C
+++ TopAngular/FillHistograms.C
@@ -264,7 +264,8 @@ void FillHistograms(TString y="2016", int sel = 0, bool isLoose=false)
   int NN = trIN->GetEntries();
 
   int nJets,nLeptons;
-  float genEvtWeight(0), bTagEvntWeight(0);
+  float genEvtWeight(0);
+  double  bTagEvntWeight(0);
   vector<float> *jetPt(0),*tau3(0),*tau2(0),*tau1(0);
   vector<float> *jetMassSub0(0), *jetMassSub1(0);
   vector<float> *jetMassSoftDrop(0);
