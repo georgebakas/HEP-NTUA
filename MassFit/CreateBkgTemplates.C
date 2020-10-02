f2474c73ade1c20519d8c7682462204e637b8665
Georgios Bakas <georgios.bakas@cern.ch>
2020-10-02 19:12:34 +0300

    test12

--- MassFit/CreateBkgTemplates.C
+++ MassFit/CreateBkgTemplates.C
@@ -1,8 +1,5 @@
-#include "QCDBkgConstants.h"
-
-void CreateBkgTemplates(TString year, TString CUT = "")
+  void CreateBkgTemplates(TString year, int icat, TString CUT = "")
 {
-  initQCDParams();
   gROOT->ForceStyle();
 
   RooMsgService::instance().setSilentMode(kTRUE);
@@ -27,98 +24,16 @@ void CreateBkgTemplates(TString year, TString CUT = "")
   RooRealVar *x = new RooRealVar("mTop","mTop",XMIN,XMAX);
   w->import(*x);
   //---- first do the data template ---------------
-  TFile *infData = TFile::Open(TString::Format("%s/Histo_Data_%s_100.root", year.Data(),year.Data()));
-  TH1F *hData = (TH1F*)infData->Get("hWt_mTop_0btag_expYield");
-  cout<<"CR Entries from data: "<<hData->GetEntries()<<endl;
-  //because of contamination we need to substract the ttbar from the CR.
-  //we do that by extracting the 0btag th1 using nominal or mtt mc
-  TFile *infTTMC = TFile::Open(TString::Format("%s/Histo_TT_NominalMC_100.root",year.Data()));  //nominal
-  //TFile *infTTMC = TFile::Open(TString::Format("%s/Histo_TT_Mtt-700toInf_100.root",year.Data())); //mtt
-  TH1F *hCR_MC = (TH1F*)infTTMC->Get("hWt_mTop_0btag_expYield");
 
   TFile *infBkg = TFile::Open(TString::Format("%s/Histo_SubdominantBkgs_100.root",year.Data()));
-  TH1F *hCR_MCSubdominant = (TH1F*)infBkg->Get("hWt_mTop_0btag_expYield");
-  TH1F *hDataBefore = (TH1F*)hData->Clone("test");
-
-  //hDataBefore->SetLineColor(kRed);
-  hData->Add(hCR_MCSubdominant,-1);
-  hData->Add(hCR_MC,-1);
-
-  hData->Rebin(2);
-
-  cout<<"After Substr: "<<hData->GetEntries()<<endl;
-  /*
-  hData->Draw();
-  hDataBefore->Draw("same");
-  hCR_MCSubdominant->SetLineColor(kGreen);
-  hCR_MC->SetLineColor(kMagenta);
-  hCR_MC->Draw("same");
-  hCR_MCSubdominant->Draw("same");
-  return;
-  */
-  RooDataHist *roohData = new RooDataHist("roohistData","roohistData",RooArgList(*x),hData);
-  TH1F *hDataJet = (TH1F*)infData->Get("hWt_jetMassSoftDrop_0btag");
-  TH1F *hDataEvt = (TH1F*)infData->Get("hWt_mJJ_0btag");
-  RooRealVar *fBkgJet = new RooRealVar("fBkgJet","fBkgJet",hDataJet->Integral()/hData->Integral());
-  RooRealVar *fBkgEvt = new RooRealVar("fBkgEvt","fBkgEvt",hDataEvt->Integral()/hData->Integral());
-  //---- QCD -----------------------------------
-  RooRealVar bQCD0("qcd_b0","qcd_b0",qcdParams[year]["qcd_b0"],0,2.);
-  RooRealVar bQCD1("qcd_b1","qcd_b1",qcdParams[year]["qcd_b1"],0,2.);
-  RooRealVar bQCD2("qcd_b2","qcd_b2",qcdParams[year]["qcd_b2"],0,2.);
-  RooRealVar bQCD3("qcd_b3","qcd_b3",qcdParams[year]["qcd_b3"],0,2.);
-  RooRealVar bQCD4("qcd_b4","qcd_b4",qcdParams[year]["qcd_b4"],0,2.);
-  RooBernstein qcd1("qcd_brn","qcd_brn",*x,RooArgList(bQCD0,bQCD1,bQCD2,bQCD3,bQCD4));
-
-
-  RooRealVar mQCD("qcd_mean" ,"qcd_mean",qcdParams[year]["qcd_mean"],130,300);
-  RooRealVar sQCD("qcd_sigma","qcd_sigma",qcdParams[year]["qcd_sigma"],10,200);
-  RooGaussian qcd2("qcd_gaus" ,"qcd_gaus",*x,mQCD,sQCD);
-
-
-  RooRealVar mWqcd("qcd_meanW", "qcd_meanW", 77, 70, 90);
-  RooRealVar sWqcd("qcd_sigmaW", "qcd_sigmaW", 20, 0, 40);
-  RooGaussian qcd3("qcd_gausW", "qcd_gausW", *x, mWqcd, sWqcd);
-
-  RooRealVar fqcd1("qcd_f1","qcd_f1",qcdParams[year]["qcd_f1"],0,1);
-  RooRealVar fqcd2("qcd_f2","qcd_f2",0.5,0,1);
-
-  RooAddPdf *qcd = new RooAddPdf("qcd_pdf","qcd_pdf",RooArgList(qcd1,qcd2), RooArgList(fqcd1));
-  qcd->Print();
-
-  //---- plots ---------------------------------------------------
-  TCanvas *canQCD = new TCanvas("Template_QCD_"+CUT,"Template_QCD_"+CUT,900,600);
-
-  RooFitResult *res = qcd->fitTo(*roohData,RooFit::Save());
-  res->Print();
-  RooPlot *frameQCD = x->frame();
-  roohData->plotOn(frameQCD);
-  qcd->plotOn(frameQCD);
-  qcd->plotOn(frameQCD,RooFit::Components("qcd_brn"),RooFit::LineColor(kRed),RooFit::LineWidth(2),RooFit::LineStyle(2));
-  qcd->plotOn(frameQCD,RooFit::Components("qcd_gaus"),RooFit::LineColor(kGreen+1),RooFit::LineWidth(2),RooFit::LineStyle(2));
-  //qcd->plotOn(frameQCD,RooFit::Components("qcd_gausW"),RooFit::LineColor(kOrange+1),RooFit::LineWidth(2),RooFit::LineStyle(2));
-  frameQCD->GetXaxis()->SetTitle("m_{t} (GeV)");
-  frameQCD->Draw();
-  gPad->Update();
-  canQCD->Print(TString::Format("%s/plots/templateResults/"+TString(canQCD->GetName())+".pdf", year.Data()));
-
-  RooArgSet *parsQCD = (RooArgSet*)qcd->getParameters(roohData);
-  parsQCD->setAttribAll("Constant",true);
-
-  w->import(*qcd);
-  //w->import(*fBkgJet);
-  //w->import(*fBkgEvt);
-  //TH1F *hMeanTop[2],*hSigmaTop[2],*hMeanW[2],*hSigmaW[2];
 
   float LUMI(0);
   if (year.EqualTo("2016")) LUMI = 35920;
   else if (year.EqualTo("2017")) LUMI = 41530;
   else if (year.EqualTo("2018")) LUMI = 59740;
-
-
-  for(int icat=0;icat<3;icat++) {
-
-    if (icat == 1)
-      continue;
+  cout<<icat<<endl;
+  if (icat == 1)
+    return;
     //infBkg = TFile::Open(TString::Format("%s/Histo_SubdominantBkgs_100.root", year.Data()));
 
     TString CAT = TString::Format("%dbtag", icat);
@@ -127,7 +42,7 @@ void CreateBkgTemplates(TString year, TString CUT = "")
     //---- do the bkg templates -------------
     if (icat == 0)
     {
-      hBkg = (TH1F *)hCR_MCSubdominant->Clone("hBkg");
+      hBkg = (TH1F *)infBkg->Get("hWt_mTop_0btag_expYield");
     }
     else if (icat == 2)
     {
@@ -142,7 +57,7 @@ void CreateBkgTemplates(TString year, TString CUT = "")
     RooGaussian pdfW("bkg_pdfW_" + CAT, "bkg_pdfW_" + CAT, *x, mW, sW);
 
     RooRealVar mBkgTop("bkg_meanTop_" + CAT, "bkg_meanTop_" + CAT, 172, 150, 180);
-    RooRealVar sBkgTop("bkg_sigmaTop_" + CAT, "bkg_sigmaTop_" + CAT, 15, 5, 40);
+    RooRealVar sBkgTop("bkg_sigmaTop_" + CAT, "bkg_sigmaTop_" + CAT, 15, 5, 30);
     RooGaussian bkgTop("bkg_pdfTop_" + CAT, "bkg_pdfTop_" + CAT, *x, mBkgTop, sBkgTop);
 
     RooRealVar bBkg0("bkg_b0_" + CAT, "bkg_b0_" + CAT, 0.5, 0, 1);
@@ -180,9 +95,9 @@ void CreateBkgTemplates(TString year, TString CUT = "")
     parsBkg->setAttribAll("Constant", true);
 
     w->import(*bkg);
-  }
 
-  w->writeToFile(TString::Format("%s/templates_Bkg_"+CUT+"100.root", year.Data()));
+
+  w->writeToFile(TString::Format("%s/templates_Bkg_"+CUT+"100_"+icat+".root", year.Data()));
 
 
 
