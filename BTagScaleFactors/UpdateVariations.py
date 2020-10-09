90607c9b10e04df4e375e23e2a8cc91b5acfacd6
George Bakas <georgios.bakas@cern.ch>
2020-10-09 15:59:34 +0200

    adding new files after re-running responses for 2017

--- BTagScaleFactors/UpdateVariations.py
+++ BTagScaleFactors/UpdateVariations.py
@@ -8,13 +8,11 @@ print(f'Argument list: {str(sys.argv)}')
 year = sys.argv[1]
 allFiles = []
 
-for ifile, file_name in enumerate(glob.iglob('/eos/cms/store/user/ipapakri/ttbar/MC/Signal/{}/variations/*.root'.format(year), recursive=True)):
+for ifile, file_name in enumerate(glob.iglob('/eos/cms/store/user/ipapakri/ttbar/MC/Signal/{}/variations/TTTo*.root'.format(year), recursive=True)):
 	#print(file_name)
-	#print(f'root -l -b -q \'myMacro.C(\"{file_name}\")\'')
-	if ifile > 28:
-		print("file {}".format(ifile))
-		split_file_name = file_name.split('/')
-		print(split_file_name[0])
-		#print(file_name)
-		os.system(f'root -l -b -q \'UpdateBTagSF.C(\"{split_file_name[-1]}\", \"{year}\")\'')
-		#break
+	print("file {}".format(ifile))
+	split_file_name = file_name.split('/')
+	print(split_file_name[-1])
+	#print(file_name)
+	os.system(f'root -l -b -q \'UpdateBTagSF.C(\"{split_file_name[-1]}\", \"{year}\")\'')
+	#break
