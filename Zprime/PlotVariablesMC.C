3854761ccc8fad43e85c529138949c630b85ae9d
Georgios Bakas <georgios.bakas@cern.ch>
2020-10-19 10:28:19 +0300

    need to rerun 1000 GeV because lack of mJJ

--- Zprime/PlotVariablesMC.C
+++ Zprime/PlotVariablesMC.C
@@ -11,24 +11,29 @@ using std::cout;
 using std::endl;
 #include "TemplateConstants.h"
 
-void PlotVariablesMC(TString file_names_Zprime, TString level, TString year, TString histoNames)
+void PlotVariablesMC(TString file_names_Zprime, TString level, TString year, TString histoNames, TString mJJCut)
 {
   initFilesMapping();
   float LUMI = luminosity["luminosity"+year];
-  TString vars[3] = {"chi", "cosTheta_0", "cosTheta_1"};
+  const int NVAR = 4;
+  TString vars[NVAR] = {"chi", "cosTheta_0", "cosTheta_1", "mJJ"};
   int cols[] = {kRed, kRed+1, kRed+3,kBlue, kBlue+1, kBlue+3,kMagenta, kMagenta+1, kMagenta+3, kGreen, kGreen+1, kGreen+3, kYellow+2,kYellow+3, kYellow+4,
                 kTeal, kTeal-3, kTeal-6};
   //open the mc ttbar file:
-  TFile *inf_fid[3], *inf_par;
-  TH1F *hSig[3];
+  TFile *inf_fid[NVAR], *inf_par;
+  TH1F *hSig[NVAR];
   if(level.EqualTo("Reco"))
   {
+    /*
     inf_fid[0] = TFile::Open(TString::Format("../TopAngular_NewBins/%s/FiducialMeasurement/EqualBinning/SignalHistograms_chi.root", year.Data()));
     inf_fid[1] = TFile::Open(TString::Format("../TopAngular_NewBins/%s/FiducialMeasurement/EqualBinning/SignalHistograms_cosTheta_0.root", year.Data()));
     inf_fid[2] = TFile::Open(TString::Format("../TopAngular_NewBins/%s/FiducialMeasurement/EqualBinning/SignalHistograms_cosTheta_1.root", year.Data()));
-    for(int ivar = 0; ivar<3; ivar++)
+    */
+    inf_fid[0] = TFile::Open(TString::Format("../TopAngular_NewBins/%s/Histo_TT_NominalMC_reduced_%s.root", year.Data(), mJJCut.Data()));
+    for(int ivar = 0; ivar<NVAR; ivar++)
     {
-      hSig[ivar] = (TH1F*)inf_fid[ivar]->Get(TString::Format("hSignal_%s", vars[ivar].Data()));
+      //hSig[ivar] = (TH1F*)inf_fid[ivar]->Get(TString::Format("hSignal_%s", vars[ivar].Data()));
+      hSig[ivar] = (TH1F*)inf_fid[0]->Get(TString::Format("hWt_%s_2btag_expYield", vars[ivar].Data()));
       float integral = hSig[ivar]->Integral();
       hSig[ivar]->Scale(1/LUMI, "width");
       hSig[ivar]->Scale(1/integral);
@@ -38,9 +43,10 @@ void PlotVariablesMC(TString file_names_Zprime, TString level, TString year, TSt
   else if(level.EqualTo("Parton") || level.EqualTo("Particle"))
   {
     inf_par = TFile::Open(TString::Format("../TopAngular_NewBins/%s/%sMeasurements/Data/OutputFile.root", year.Data(), level.Data()));
-    for(int ivar = 0; ivar<3; ivar++)
+    for(int ivar = 0; ivar<NVAR; ivar++)
     {
-      hSig[ivar] = (TH1F*)inf_par->Get(TString::Format("hUnfoldFinal_%s", vars[ivar].Data()));
+      //hSig[ivar] = (TH1F*)inf_par->Get(TString::Format("hUnfoldFinal_%s", vars[ivar].Data()));
+      hSig[ivar] = (TH1F*)inf_par->Get(TString::Format("hTheoryFinal_%s", vars[ivar].Data()));
       float integral = hSig[ivar]->Integral();
       hSig[ivar]->Scale(1/LUMI, "width");
       hSig[ivar]->Scale(1/integral);
@@ -58,9 +64,9 @@ void PlotVariablesMC(TString file_names_Zprime, TString level, TString year, TSt
   TObjArray *t_names = histoNames.Tokenize(",");
   //tx->Print();
   //t_names->Print();
-  TH1F *hZ[3];
-  TCanvas *can[3];
-  for(int ivar= 0; ivar<3; ivar++)
+  TH1F *hZ[NVAR];
+  TCanvas *can[NVAR];
+  for(int ivar= 0; ivar<NVAR; ivar++)
   {
     //cout<<year<<"/"<<((TObjString *)(tx->At(i)))->String()<<endl;
     can[ivar] = new TCanvas(TString::Format("can_%s", vars[ivar].Data()), TString::Format("can_%s", vars[ivar].Data()), 800,600);
@@ -70,8 +76,9 @@ void PlotVariablesMC(TString file_names_Zprime, TString level, TString year, TSt
     hSig[ivar]->Draw();
     for (Int_t i = 0; i < tx->GetEntries(); i++)
     {
+      cout<<year+"/"+((TObjString *)(tx->At(i)))->String()<<endl;
       TFile *infZprime = TFile::Open(year+"/"+((TObjString *)(tx->At(i)))->String());
-      hZ[ivar] = (TH1F*)infZprime->Get(TString::Format("h%s_%s", level.Data(), vars[ivar].Data()));
+      hZ[ivar] = (TH1F*)infZprime->Get(TString::Format("h%s_%s_%s", level.Data(), vars[ivar].Data(), mJJCut.Data()));
       hZ[ivar]->SetName(((TObjString *)(t_names->At(i)))->String());
       hZ[ivar]->SetTitle(((TObjString *)(t_names->At(i)))->String());
       hZ[ivar]->SetLineColor(cols[i]);
--- Zprime/RunPlots.py
+++ Zprime/RunPlots.py
@@ -7,11 +7,12 @@ print(f'Argument list: {str(sys.argv)}')
 
 year = sys.argv[1]
 level = sys.argv[2]
+mJJCut = sys.argv[3]
 allFiles = []
 histo_names = []
 
-for ifile, file_name in enumerate(glob.iglob('{}/*.root'.format(year), recursive=True)):
-    #print('file: {}'.format(file_name))
+for ifile, file_name in enumerate(glob.iglob('{}/HistoMassWindows*{}*.root'.format(year, mJJCut), recursive=True)):
+    print('file: {}'.format(file_name))
     file_to_append = file_name.split('/')
     allFiles.append(file_to_append[-1])
     split_file_in_underscores = file_to_append[-1].split('_')
@@ -20,5 +21,5 @@ for ifile, file_name in enumerate(glob.iglob('{}/*.root'.format(year), recursive
     histo_names.append(mass_name+"_"+width_name)
 
 #print(histo_names)
-os.system(f'root -l \'PlotVariablesMC.C(\"{allFiles}\", \"{level}\",\"{year}\", \"{histo_names}\")\'')
+os.system(f'root -l \'PlotVariablesMC.C(\"{allFiles}\", \"{level}\",\"{year}\", \"{histo_names}\", \"{mJJCut}\")\'')
     #break
