583dfb55fd2da964317fa54c814a90f304f6b18e
George Bakas <georgios.bakas@cern.ch>
2020-11-13 08:17:23 +0100

    Code for ps and pdf weights. PDF weights need more tuning

--- VariationHandling/TemplateConstants.h
+++ VariationHandling/TemplateConstants.h
@@ -5,16 +5,19 @@ map<TString, map<TString, float>> XSECAll;
 map<TString, float> floatConstants;
 map<TString, float> topTaggerConstants;
 map<TString, float> luminosity;
+map<TString, float> luminosityCR;
 map<TString, TString> eospath;
+map<int, TString> ps_weights;
 
 void initFilesMapping()
 {
 
 	eospath.insert(pair<TString,TString>("2016","/eos/cms/store/user/gbakas/ttbar/topTagger/mc-2016/variations/combined/"));
-	eospath.insert(pair<TString,TString>("2017","/eos/cms/store/user/gbakas/ttbar/topTagger/mc-2016/variations/combined/"));
-	eospath.insert(pair<TString,TString>("2018","/eos/cms/store/user/gbakas/ttbar/topTagger/mc-2016/variations/combined/"));
+	eospath.insert(pair<TString,TString>("2017","/eos/cms/store/user/gbakas/ttbar/topTagger/mc-2017/variations/combined/"));
+	eospath.insert(pair<TString,TString>("2018","/eos/cms/store/user/gbakas/ttbar/topTagger/mc-2018/variations/combined/"));
 
 	map<TString, float> XSEC_ = {{"TT", 832.},
+															{"TTJets", 832.},
 														  {"TTToHadronic", 377.96},
 															{"TTToSemiLeptonic", 365.34},
 															{"TTTo2L2Nu", 88.29}};
@@ -35,5 +38,26 @@ void initFilesMapping()
 	luminosity.insert(pair<TString,float>("luminosity2016", 35920));
 	luminosity.insert(pair<TString,float>("luminosity2017", 41530));
 	luminosity.insert(pair<TString,float>("luminosity2018", 59740));
+
+	luminosityCR.insert(pair<TString,float>("luminosity2016", 1670));
+	luminosityCR.insert(pair<TString,float>("luminosity2017", 41530));
+	luminosityCR.insert(pair<TString,float>("luminosity2018", 59740));
+
+	//ps weights
+	ps_weights.insert(pair<int, TString>(0, "nom0"));
+	ps_weights.insert(pair<int, TString>(1, "nom1"));
+	ps_weights.insert(pair<int, TString>(2, "isrRedHi"));
+	ps_weights.insert(pair<int, TString>(3, "fsrRedHi"));
+	ps_weights.insert(pair<int, TString>(4, "isrRedLo"));
+	ps_weights.insert(pair<int, TString>(5, "fsrRedLo"));
+	ps_weights.insert(pair<int, TString>(6, "isrDefHi"));
+	ps_weights.insert(pair<int, TString>(7, "fsrDefHi"));
+	ps_weights.insert(pair<int, TString>(8, "isrDefLo"));
+	ps_weights.insert(pair<int, TString>(9, "fsrDefLo"));
+	ps_weights.insert(pair<int, TString>(10, "isrConHi"));
+	ps_weights.insert(pair<int, TString>(11, "fsrConHi"));
+	ps_weights.insert(pair<int, TString>(12, "isrConLo"));
+	ps_weights.insert(pair<int, TString>(13, "fsrConLo"));
+
 
 }
