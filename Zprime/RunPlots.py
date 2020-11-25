076d152b07a56cf1dffc28479b7d7513df89f0cf
georgebakas <georgios.bakas@cern.ch>
2020-11-25 14:03:17 +0200

    Have to change templateConstants.h in Zprime so that I run on medium WPs

--- Zprime/RunPlots.py
+++ Zprime/RunPlots.py
@@ -18,5 +18,5 @@ for ifile, file_name in enumerate(glob.iglob('{}/HistoCutFlowJetMassSoftDrop_*.r
     width_name = split_file_in_underscores[3]
     histo_names = mass_name+"_"+width_name
     #os.system(f'root -l -q \'PlotCutFlow.C(\"{year}\", \"{file_to_append[-1]}\",\"{histo_names}\")\'')
-    os.system(f'root -l \'PlotCutFlow_JetMassSoftDrop.C(\"{year}\", \"{file_to_append[-1]}\",\"{histo_names}\")\'')
+    os.system(f'root -l -b -q \'PlotCutFlow_JetMassSoftDrop.C(\"{year}\", \"{file_to_append[-1]}\",\"{histo_names}\")\'')
     #break
